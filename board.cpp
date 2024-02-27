e723c53e40b8a83f9125f9b31ab2f01b0ec009f4
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-02-27 16:50:03 -0500

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -2,7 +2,7 @@
 #include "board.hpp"
 Board::Board() {} //the constructor for board
 
-Board::~Board(){} //the destructor for board
+Board::~Board(){}  //the destructor for board
 
 ostream& Board::print (ostream& boardOutput) {
    for (int k = 0; k < 13; k++) {
@@ -12,9 +12,38 @@ ostream& Board::print (ostream& boardOutput) {
    return boardOutput;
 }
 
-void Board::startTurn(Player*) {
-    Player*;
+void Board::startTurn(Player* playerMoving) {
+    currentPlayer = playerMoving;
     towerCounter = 0;
-    towerDetector = 0;
+    towerDetector[0] = 0;
+    towerDetector[1] = 0;
+    towerDetector[2] = 0;
 }
 
+bool Board::move(int columnVal) {
+columnVal = 3;
+if (backBone[columnVal]->getState() == 1 || backBone[columnVal]->getState() == 2 ) {
+    return false;
+     }
+else if (towerCounter == 3) {
+    return false;
+    }
+else {
+ backBone[columnVal] -> move();
+    return true;
+  }
+}
+
+void Board::stop() {
+for (int k = 0; k < towerCounter; k++) {
+    int columnNum = towerDetector[k];
+    backBone[columnNum]->stop(currentPlayer);
+    }
+}
+
+void Board::bust() {
+for (int k = 0; k < towerCounter; k++) {
+    int columnNum = towerDetector[k];
+    backBone[columnNum]->bust(); //the function does not do anything yet because there's no instructions for it
+    }
+}
